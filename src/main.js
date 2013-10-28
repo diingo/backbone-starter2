3ff88c42fcf5b834cbd0a37965f9d80ba532359b
Gene Le <gene.gkl@gmail.com>
2013-10-28 12:45:53 -0500

    Add initialize function to bb view

--- src/main.js
+++ src/main.js
@@ -1,29 +1,14 @@
-var ShowCommentsView = Backbone.View.extend({
 
-  render: function () {
-    // Empty the comments content div so that we don't duplicate comments
-    $('#show-comments .content').empty();
-
-    // For each comment, generate html and add it to the page
-    for (var i = 0; i < comments.length; i += 1) {
-      var newCommentHtml = commentTemplate( comments[i] );
-      $('#show-comments .content').append(newCommentHtml);
-    }
-
-    return this;
-  }
-});
-var commentsView = new ShowCommentsView();
 
 // Since the comment template never changes, we can keep this global
 var commentTemplateHtml = $('#templates .comment').html()
 var commentTemplate = _.template(commentTemplateHtml);
 
-var comments = [
-  { username: 'xXx_coolkid_xXx', message: 'dude ur almost as kool as me' },
-  { username: 'top_redditr', message: 'old' },
-  { username: 'l33tschool', message: 'w/e man u cnt b33t m3' }
-];
+// var comments = [
+//   { username: 'xXx_coolkid_xXx', message: 'dude ur almost as kool as me' },
+//   { username: 'top_redditr', message: 'old' },
+//   { username: 'l33tschool', message: 'w/e man u cnt b33t m3' }
+// ];
 
 // var render = function () {
   // Empty the comments content div so that we don't duplicate comments
@@ -50,3 +35,30 @@ $('.new-comment button').on('click', function (e) {
 
 // Render initial comments on page load
 // render();
+
+var ShowCommentsView = Backbone.View.extend({
+
+  initialize: function(options){
+    this.comments = [
+      {       { username: 'xXx_coolkid_xXx', message: 'dude ur almost as kool as me' },
+      { username: 'top_redditr', message: 'old' },
+      { username: 'l33tschool', message: 'w/e man u cnt b33t m3' }
+      {},
+      {}
+    ];
+  },
+  render: function () {
+    // Empty the comments content div so that we don't duplicate comments
+    $('#show-comments .content').empty();
+
+    // For each comment, generate html and add it to the page
+    for (var i = 0; i < comments.length; i += 1) {
+      var newCommentHtml = commentTemplate( comments[i] );
+      $('#show-comments .content').append(newCommentHtml);
+    }
+
+    return this;
+  }
+});
+var commentsView = new ShowCommentsView();
+commentsView.render();
