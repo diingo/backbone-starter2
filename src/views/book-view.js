d22cb1b7bb2ccc778bb32215601b05b9d3eced21
Gene Le <gene.gkl@gmail.com>
2013-11-03 13:28:54 -0600

    on ex 5 - add book class using className in book-view, also tried adding click event using events: in book view and buy function

--- src/views/book-view.js
+++ src/views/book-view.js
@@ -4,8 +4,16 @@
   var bookTemplate = _.template(bookTemplateHtml);
 
   window.BookView = Backbone.View.extend({
-    buy: function () {
-      // TODO
+    // buy: function () {
+    //   // TODO
+    // },
+
+    className: 'book',
+    events: {
+      "click .buy": "buy"
+    },
+    buy: function(e) {
+      console.log("clicked buy?");
     },
     render: function () {
       var newBookHtml = bookTemplate( this.model.toJSON() );
