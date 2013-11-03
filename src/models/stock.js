012451810d052d6dba924d6c2a1b92b0b73a3a6c
Gene Le <gene.gkl@gmail.com>
2013-11-03 11:54:20 -0600

    add color to pricechange

--- src/models/stock.js
+++ src/models/stock.js
@@ -1,11 +1,26 @@
 (function () {
-
+//ASK - why price not update like it does on console when printed on index.html
   window.Stock = Backbone.Model.extend({
+    defaults: {
+      priceset: false,
+      initprice: 0,
+      recentchange: 0
+    },
     updatePrice: function (newPrice) {
-      newPriceFloat = parseFloat(newPrice);
-      console.log('Updating', this.get('name'), 'price to:', newPriceFloat);
+      var newPriceFloat = parseFloat(newPrice);
+      var initPrice = this.get("initprice"); 
+      // console.log('Updating', this.get('name'), 'price to:', newPriceFloat);
       // TODO
       this.set({ price: newPriceFloat });
+
+      if ( this.get('priceset') === false ) {
+        this.set({ initprice: newPriceFloat,
+          priceset: true
+        });
+      }
+      
+      this.set({ recentchange: Math.abs(newPriceFloat - initPrice) });
+    
     }
   });
 
--- src/views/stock-view.js
+++ src/views/stock-view.js
@@ -1,8 +1,29 @@
 (function () {
 
+  var stockTemplateHtml = $('#templates .stock').html();
+  var stockTemplate = _.template(stockTemplateHtml);
+
   window.StockView = Backbone.View.extend({
-    className: 'stock'
+    className: 'stock',
     // TODO
+    initialize: function (){
+      this.listenTo(this.model, 'change', this.render);
+      console.log(this, this.model)
+    },
+    render: function () {
+      var newStockHtml = stockTemplate( this.model.toJSON() );
+
+      $(this.el).html(newStockHtml);
+
+      var currentPrice = this.model.get("price");
+      var prevPrice = this.model.previous("price")
+      if ( currentPrice > prevPrice ) {
+        $(this.el).find('.price').addClass("up");
+      } else {
+        $(this.el).find(".price").addClass("down");
+      }
+    }
+
   });
 
 })();
